f5270477ef76630a434c84878939af2171cdadb4
Jornvanbeek <34307848+Jornvanbeek@users.noreply.github.com>
2017-12-06 17:13:34 +0100

    Update Tank sizing project.py

--- Tank sizing project.py
+++ Tank sizing project.py
@@ -1,22 +1,45 @@
 from math import *
 import matplotlib.pyplot as plt
-
-L = 1.
-v = 0.3
-R = 0.5
-t1 = 0.0001
+#std parameters
+vol = 1.244070691
+R = 0.6
+t1 = 0.0013
+p = 1000.
+m = 1967.3 #dry+fuel= 1337+630.3 = 1967.3
+g = 9.81
+rho = 2700.
+gforce = 6.5
+#material properties
 E = 27.*10**9
-I = 123456.
-A = 0.9
-p = 10000.
+v = 0.3
 
+#calculated parameters
+A = 2 * pi * R *t1
+I = pi*(R**3)*t1
+L = 1.5
 
-k = 2*sqrt(12*(L**4)*(1-(v**2))/((pi**4)*(R**2)*(t1**2)))
+#launch loads
+stress = m*g*gforce/A
+
+#fucking difficult formulas concerning buckling
+k = 2*(12*(L**4)*(1-(v**2))/((pi**4)*(R**2)*(t1**2)))**0.5
+
 Q = (p/E)*((R/t1)**2)
-stresscr = ((pi**2) * E * I / (A* (L**2)))
-stressbuckl = ((1.983-0.983*(e**(-23.14*Q)))*k*(pi**2)*E*((t1/L)**2)/(12*(1-v**2)))
 
-print stresscr, "MPa"
-print stressbuckl, "MPa"
+column = ((pi**2) * E * I / (A* (L**2)))
+
+shell = ((1.983-0.983*(e**(-23.14*Q)))*k*(pi**2)*E*((t1/L)**2)/(12*(1-v**2)))
+
+
+mscol = column/stress -1
+msshell = shell/stress - 1
+
+#printing values
+print column, "column"
+print shell, "shell"
+print stress
+print mscol, "margin column"
+print msshell, "margin shell"
+
 
 
